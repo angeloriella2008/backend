2409831dc5f84756f969c90275b9e5904a3af499
Maac26 <achillemazameza27@gmail.com>
2026-09-25 13:55:38 +0200

    app with backend

--- test_gsi.dart
+++ test_gsi.dart
@@ -1,5 +1,5 @@
-import 'package:google_sign_in/google_sign_in.dart';
-void main() {
-  final gsi = GoogleSignIn();
-  print(gsi);
-}
+// import 'package:google_sign_in/google_sign_in.dart';
+// void main() {
+//   final gsi = GoogleSignIn();
+//   print(gsi);
+// }
